3484d23187336bdc41fac5e4c0900a3d55a774f6
ShipIM <ilya.shipunov@gmail.com>
2023-11-17 16:08:03 +0300

    component scan fix

--- application/src/main/java/com/example/Application.java
+++ application/src/main/java/com/example/Application.java
@@ -1,14 +1,11 @@
 package com.example;
 
-import com.example.controller.CourseController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
-@ComponentScan(basePackageClasses = CourseController.class)
 public class Application {
 
     public static void main(String[] args) {
