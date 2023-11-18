17a7d248d44caec4e6f3a2008ad8eebbd681bf4d
Vadim <92179377+Zhuzhalica@users.noreply.github.com>
2023-11-18 23:02:18 +0500

    create service

--- application/src/main/java/com/example/service/CourseService.java
+++ application/src/main/java/com/example/service/CourseService.java
@@ -2,6 +2,7 @@ package com.example.service;
 
 import com.example.model.entity.Course;
 import com.example.repository.CourseRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -15,5 +16,9 @@ public class CourseService {
 
     public List<Course> getCourses() {
         return courseRepository.findAll();
+    }
+
+    public Course getById(long id) {
+        return courseRepository.findById(id).orElseThrow(()->new EntityNotFoundException("Курса с таким id не существует"));
     }
 }
---
+++ application/src/main/java/com/example/service/SubjectService.java
@@ -0,0 +1,34 @@
+package com.example.service;
+
+import com.example.model.entity.Subject;
+import com.example.repository.SubjectRepository;
+import com.querydsl.core.types.Predicate;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Service
+@RequiredArgsConstructor
+public class SubjectService {
+    private final SubjectRepository subjectRepository;
+    private final CourseService courseService;
+
+    public List<Subject> findAll(Predicate predicate)
+    {
+        var sort = Sort.by(Sort.Order.asc("name"));
+        List<Subject> result = new ArrayList<>();
+        subjectRepository.findAll(predicate, sort).forEach(result::add);
+        return result;
+    }
+
+    public Subject create(Subject subject)
+    {
+        subject.setCourse(courseService.getById(subject.getCourse().getId()));
+
+        return subjectRepository.save(subject);
+    }
+}
