d74422cd87d14b19fcef82ee4b19de21ec86fa14
ShipIM <ilya.shipunov@gmail.com>
2023-11-20 20:12:58 +0300

    resource controller changes

--- application/src/main/java/com/example/controller/ResourceController.java
+++ application/src/main/java/com/example/controller/ResourceController.java
@@ -1,15 +1,19 @@
 package com.example.controller;
 
+import com.example.constraint.ResourceTypeConstraint;
 import com.example.dto.mapper.ResourceMapper;
 import com.example.dto.resource.CreateResourceRequestDto;
 import com.example.dto.resource.ResourceResponseDto;
 import com.example.dto.resource.ResourceViewResponseDto;
 import com.example.model.entity.QResource;
 import com.example.model.entity.Resource;
+import com.example.model.enumeration.ResourceType;
 import com.example.service.ResourceService;
+import com.querydsl.core.types.ExpressionUtils;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -27,14 +31,14 @@ public class ResourceController {
     private final ResourceService resourceService;
     private final ResourceMapper resourceMapper;
 
-    @GetMapping("/resources/{resource}")
+    @GetMapping("/resources/{id}")
     @Operation(description = "Получить существующий ресурс по идентификатору")
     public ResourceResponseDto getResource(
             @PathVariable
             @Pattern(regexp = "^(?!0+$)\\d{1,19}$",
                     message = "Идентификатор ресурса должен быть положительным числом типа long")
-            String resource) {
-        Resource res = resourceService.getResourceById(Long.parseLong(resource));
+            String id) {
+        Resource res = resourceService.getResourceById(Long.parseLong(id));
 
         return resourceMapper.mapToResourceDto(res);
     }
@@ -53,15 +57,21 @@ public class ResourceController {
         return resourceMapper.mapToResourceDto(resource);
     }
 
-    @GetMapping("/subjects/{subject}/resources")
+    @GetMapping("/subjects/{id}/resources")
     @Operation(description = "Получить все существующие ресурсы")
     public List<ResourceViewResponseDto> getResources(
             @PathVariable
             @Pattern(regexp = "^(?!0+$)\\d{1,19}$",
                     message = "Идентификатор предмета должен быть положительным числом типа long")
-            String subject) {
+            String id,
+            @RequestParam
+            @ResourceTypeConstraint(message = "Неизвестный тип ресурса")
+            @NotBlank(message = "Необходимо указать тип ресурса")
+            String resourceType) {
         List<Resource> resources = resourceService.getResources(
-                QResource.resource.subject.id.eq(Long.parseLong(subject))
+                ExpressionUtils.allOf(
+                        QResource.resource.subject.id.eq(Long.parseLong(id)),
+                        QResource.resource.resourceType.eq(ResourceType.valueOf(resourceType)))
         );
 
         return resourceMapper.mapResourceListToViewDtoList(resources);
