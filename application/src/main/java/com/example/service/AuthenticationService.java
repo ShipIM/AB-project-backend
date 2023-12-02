0bd1669d6908a431306fd07f4404e4e1eff49001
Vadim <92179377+Zhuzhalica@users.noreply.github.com>
2023-12-02 22:59:24 +0500

    in work

--- application/src/main/java/com/example/service/AuthenticationService.java
+++ application/src/main/java/com/example/service/AuthenticationService.java
@@ -12,6 +12,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -24,6 +25,7 @@ public class AuthenticationService {
     private final AuthenticationManager authenticationManager;
     private final UserMapper userMapper;
 
+    @Transactional
     public AuthenticationResponseDto register(User user) {
         user.setRole(Role.USER);
         user.setPassword(passwordEncoder.encode(user.getPassword()));
