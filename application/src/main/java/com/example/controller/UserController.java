612ab875b63a24ecc550742d36303f851986ba67
ShipIM <ilya.shipunov@gmail.com>
2023-12-02 19:51:51 +0300

    some changes

--- application/src/main/java/com/example/controller/UserController.java
+++ application/src/main/java/com/example/controller/UserController.java
@@ -1,6 +1,5 @@
 package com.example.controller;
 
-import com.example.constraint.UserStatusConstraint;
 import com.example.dto.mapper.UserMapper;
 import com.example.dto.page.request.PagingDto;
 import com.example.dto.status.request.UpdateStatusRequestDto;
@@ -14,7 +13,6 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
-import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -28,7 +26,7 @@ public class UserController {
     private final UserService userService;
     private final UserMapper userMapper;
 
-    @PreAuthorize("hasAuthority('ADMIN')")
+    @PreAuthorize("hasRole('ADMIN')")
     @Operation(description = "Получить всех существующих пользователей")
     @GetMapping
     public Page<UserResponseDto> getUsers(@Valid PagingDto pagingDto) {
@@ -37,7 +35,7 @@ public class UserController {
         return users.map(userMapper::mapToDto);
     }
 
-    @PreAuthorize("hasAuthority('ADMIN')")
+    @PreAuthorize("hasRole('ADMIN')")
     @Operation(description = "Удалить существующего пользователя по id")
     @PatchMapping("/{userId}")
     public void deleteUser(
